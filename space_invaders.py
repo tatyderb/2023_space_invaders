1c47f0d66db65f00706a636682ec9480d3300352
tatyderb <tatyderb@gmail.com>
2023-09-27 14:15:48 +0300

    draw player

--- space_invaders.py
+++ space_invaders.py
@@ -9,35 +9,45 @@ FPS = 24    # frame per second
 clock = pg.time.Clock()
 
 # изображения
-bg_img = pg.image.load('../src/background.png')
-icon_img = pg.image.load('../src/ufo.png')
+bg_img = pg.image.load('src/background.png')
+icon_img = pg.image.load('src/ufo.png')
 
 display = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_icon(icon_img)
 pg.display.set_caption('Космическое вторжение')
 
 sys_font = pg.font.SysFont('arial', 34)
-font = pg.font.Font('../src/04B_19.TTF', 48)
+font = pg.font.Font('src/04B_19.TTF', 48)
 
 # display.fill('blue', (0, 0, screen_width, screen_height))
-display.blit(bg_img, (0, 0))
+display.blit(bg_img, (0, 0))        # image.tr
 
 text_img = sys_font.render('Score 123', True, 'white')
-display.blit(text_img, (100, 50))
+# display.blit(text_img, (100, 50))
 
 game_over_text = font.render('Game Over', True, 'red')
 w, h = game_over_text.get_size()
-display.blit(game_over_text, (screen_width/2 - w/2, screen_height / 2 - h/2))
+# display.blit(game_over_text, (screen_width/2 - w/2, screen_height / 2 - h/2))
 
 # игрок
-player_img = pg.image.load('../src/player.png')
+player_img = pg.image.load('src/player.png')
 player_width, player_height = player_img.get_size()
-
+player_gap = 10
+player_velocity = 1
+player_x = screen_width/2 - player_width/2
+player_y = screen_height  - player_height - player_gap
 
 running = True
 while running:
+    # изменение модели
+    player_x += player_velocity
+
+    # redraw
+    display.blit(bg_img, (0, 0))
+    display.blit(player_img, (player_x, player_y))
     pg.display.update()
 
+    # обработка событий
     for event in pg.event.get():
         # нажали крестик на окне
         if event.type == pg.QUIT:
@@ -47,8 +57,7 @@ while running:
             # нажали на q - quit
             if event.key == pg.K_q:
                 running = False
-            if event.key == pg.K_SPACE:
-                display.blit(bg_img, (0, 0))
+
 
     clock.tick(FPS)
 
