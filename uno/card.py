14abaac5b8fb8b35d78aa42965fd93a91c6dedda
tatyderb <tatyderb@gmail.com>
2023-10-25 15:35:53 +0300

    class Card - описание одной карты

---
+++ uno/card.py
@@ -0,0 +1,38 @@
+import pytest
+
+COLORS = ['red', 'green', 'blue', 'yellow']
+NUMBERS = list(range(10)) + list(range(1, 10))
+COLOR_LETTERS = {'r': 'red', 'g': 'green', 'b': 'blue', 'y': 'yellow'}
+
+
+class Card:
+    def __init__(self, color, number):
+        if color in COLORS:
+            self.color = color  # 'red' vs 'r'
+        else:
+            raise ValueError(f'Wrong color {color}')
+
+        if 0 <= number <= 9:
+            self.number = number
+        else:
+            raise ValueError(f'Wrong number {number}')
+
+    def __repr__(self):
+        return f'{self.color[0]}{self.number}'
+
+    def __eq__(self, other):
+        return self.color == other.color and self.number == other.number
+
+    @staticmethod
+    def create(text: str):
+        """ По тексту вида 'r4' возвращается карта Card('red', 4)."""
+        letter = COLOR_LETTERS.get(text[0], None)
+        number = int(text[1:])
+        return Card(letter, number)
+
+    @staticmethod
+    def card_list(text: str):
+        """ Из строки вида 'y9 r9 y0 y1' возвращает список соответствующих карт."""
+        return [Card.create(word) for word in text.split()]
+
+
---
+++ uno/test/test_card.py
@@ -0,0 +1,27 @@
+import pytest
+
+from card import Card
+
+
+def test_card():
+    c = Card('blue', 9)
+    assert c.color == 'blue'
+    assert c.number == 9
+
+def test_create():
+    c = Card.create('r4')
+    # assert c == Card('red', 4)
+    assert c == Card('red', 4)
+    assert str(c) == 'r4'
+
+def test_create_wrong_color():
+    with pytest.raises(ValueError):
+        c = Card.create('a4')
+
+def test_create_wrong_number():
+    with pytest.raises(ValueError):
+        c = Card.create('r14')
+
+def test_create_card_list():
+    text = "y9 r9 y0 y1"
+    assert Card.card_list(text) == [Card("yellow", 9), Card('red', 9), Card('yellow', 0), Card('yellow', 1)]
