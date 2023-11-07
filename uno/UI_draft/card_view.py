db174fc1ffe042e189037a8eb90097f5b29508b8
tatyderb <tatyderb@gmail.com>
2023-11-08 01:23:34 +0300

    Размер карты сделали подходящим под поле.

--- uno/UI_draft/card_view.py
+++ uno/UI_draft/card_view.py
@@ -7,10 +7,12 @@ from card import Card
 class CardView:
     """ Представление класса Card."""
     bg_image = pygame.image.load(RSC['img']['card'].format('back'))
+    size = (width, height) = GEOM['card']
 
     def __init__(self, card: Card, x: int, y: int, face_up: bool = True):
         self.card = card
-        self.image = pygame.image.load(RSC['img']['card'].format(repr(card)))
+        img = pygame.image.load(RSC['img']['card'].format(repr(card)))
+        self.image = pygame.transform.scale(img, self.size)
         self.face_up = face_up
         self.pos = (x, y)
 
